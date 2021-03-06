7f740e3924ff4f4d1f5cc3fc725dad11430fb83a
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-03-06 18:10:46 -0300

    Create e4.php

---
+++ arquivos/e4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exemplo</title>
+</head>
+<body>
+    <h1>desenvolvendo um site com php</h1>
+    <?php
+        $_nome = "Murilo";
+        $_escola = "ETEC";
+        $_curso = "desenvolvimento de sistemas";
+        echo "Meu nome é $_nome, estudo na $_escola e faço o curso de $_curso"
+    ?>
+</body>
+</html>
