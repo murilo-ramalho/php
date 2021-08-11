045cfca2e99d171cff7b842508340d53871c2538
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-08-11 20:40:44 -0300

    update

---
+++ arquivos/agosto/formulario.php
@@ -0,0 +1,8 @@
+<?php
+$Ccurso = $_POST["inputC"];
+$Ncurso = $_POST["inputN"];
+$Hcurso = $_POST["inputH"];
+$Vcurso = $_POST["inputV"];
+echo "<h1> Dados Recebidos </h1>";
+echo "<p>O nome do curso é $Ncurso, tem $Hcurso horas e custas R$ $Vcurso</p>"
+?>
---
+++ arquivos/agosto/pagina01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--Murilo Ramalho da Mata-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fomulario test</title>
+</head>
+<body>
+    <h1>titulo</h1>
+    <h2>2 titulo</h2>
+    <?php
+
+
+    ?>
+</body>
+</html>
